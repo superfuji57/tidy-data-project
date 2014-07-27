1367f0504ea156aa67661fa525697efedf09fd80
Andrew Won <andy.won@gmail.com>
2014-07-27 15:27:14 -0400

    First commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,22 @@
-if (!file.exists('data')) {
-        dir.create('data')
-}
+if (!file.exists('data')) {dir.create('data')}
 fileUrl <- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
 download.file(fileUrl,destfile="./data/dataset.zip",method="curl")
 dateDownloaded <- date()
-setwd('./dropbox/class/03-cleaning data/project')
 library(tools)
 unzip('./data/dataset.zip', exdir = './data/')
-activity.labels <- read.table('./uci har dataset/activity_labels.txt', sep=' ',
+activity.labels <- read.table('./data/uci har dataset/activity_labels.txt', sep=' ',
                             col.names=c('index', 'activity'))
-features <- read.table('./uci har dataset/features.txt', sep=' ', 
+features <- read.table('./data/uci har dataset/features.txt', sep=' ', 
                        col.names=c('index', 'feature'))
 
-test.files <- list_files_with_type('./uci har dataset/test', type='data')
-test.files <- c(test.files, list_files_with_type('./uci har dataset/test/Inertial Signals',
-                                                 type='data'))
+#extract full names of files to be used. Excludes Inertial Signal folders as they
+#are not relevant to the project
+test.files <- list_files_with_type('./data/uci har dataset/test', type='data')
 test.cols <- c(file_path_sans_ext(basename(test.files)))
-train.files <- list_files_with_type('./uci har dataset/train', type='data')
-train.files <- c(train.files, list_files_with_type('./uci har dataset/train/Inertial Signals',
-                                                 type='data'))
+train.files <- list_files_with_type('./data/uci har dataset/train', type='data')
 train.cols <- c(file_path_sans_ext(basename(train.files)))
 
+#loop over file names to read into data frames
 for(i in 1:length(test.files)){
         oname = test.cols[i]
         assign(oname, read.table(test.files[i]))
@@ -29,14 +25,22 @@ for(i in 1:length(train.files)){
         oname = train.cols[i]
         assign(oname, read.table(train.files[i]))
 }
-m.X <- rbind(X_train, X_test)
-names(m.X) <- tolower(features$feature)
-m.subject <- rbind(subject_train, subject_test)
-m.labels <- rbind(y_train, y_test)
-m.X$activity <- as.factor(tolower(activity.labels[m.labels[,],2]))
-m.X$subject <- as.factor(m.subject[,1])
-m.X2 <- m.X[,grepl('*mean*|*std()*|subject|activity', names(m.X))]
-m.X2 <- m.X2[,!grepl('Freq', names(m.X2))]
 
+#use rbind to combine rows of testing and training data
+m.X <- rbind(X_train, X_test)
+names(m.X) <- tolower(features$feature) #apply lower case feature names to col titles
+m.subject <- rbind(subject_train, subject_test) #merge subject row
+m.labels <- rbind(y_train, y_test) #merge and add labels of variables
+#add factored variables
+m.X$activity <- as.factor(tolower(activity.labels[m.labels[,],2]))
+m.X$subject <- as.factor(m.subject[,1]) 
+
+#get only mean and std
+m.X2 <- m.X[,grepl('*mean*|*std()*|subject|activity', names(m.X))]
+m.X2 <- m.X2[,!grepl('Freq', names(m.X2))] #remove meanFreq variables
+
+#view means by subject and activity
 tidy <- aggregate(m.X2[,1:66], by=list(subject=m.X2$subject, activty=m.X2$activity), 
                   FUN=mean, na.rm=TRUE)
+#export file as txt
+write.table(tidy, file='tidy.txt', sep='/t')
